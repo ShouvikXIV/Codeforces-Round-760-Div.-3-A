53b0be9eb6d4b6a23162dc40939c92d7bb2ca4c8
shouvik-barua <85792552+shouvik-barua@users.noreply.github.com>
2021-12-15 01:00:26 +0600

    Update 760A.cpp

--- 760A.cpp
+++ 760A.cpp
@@ -9,7 +9,6 @@ int main()
 	#endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
 	int t;
 	cin>>t;
 	for(int i = 0;i<t;i++)
@@ -21,25 +20,7 @@ int main()
 			cin>>k;
 			v.push_back(k);
 		}
-		// for(int f = 0;f<7;f++)
-		// {
 		int f = 0;
-		// cout<<v[f]<<'\n';
-		// cout<<v[f+1]<<'\n';
-		// cout<<v[f+2]<<'\n';
-		// cout<<v[f+3]<<'\n'; 
-		// if(v[f]==v[f+1]==v[f+2]||v[f+1]==v[f+2])
-		// {
-		// 	cout<<v[f]<<' '<<v[f+1]<<' '<<v[f+2]<<'\n';
-		// }
-		// else if(v[f]+v[f+2]>v[f+3])
-		// {
-		// 	cout<<v[f]<<' '<<v[f+1]<<' '<<v[f+2]<<'\n';
-		// }
-		// else if(v[f]+v[f+2]<=v[f+3])
-		// {
-		// 	cout<<v[f]<<' '<<v[f+1]<<' '<<v[f+3]<<'\n';
-		// }
 		if(v[f]+v[f+1]+v[f+2]==v[6])
 		{
 			cout<<v[f]<<' '<<v[f+1]<<' '<<v[f+2]<<'\n';
@@ -48,16 +29,6 @@ int main()
 		{
 			cout<<v[f]<<' '<<v[f+1]<<' '<<v[f+3]<<'\n';
 		}
-		
 	}
-			
-
-
-
-
-
-
-
-
 	return 0;
 }
